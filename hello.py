35e60ccef039fe850878664ae951f96747439973
haha110110 <boyang.afma@gmail.com>
2021-08-16 22:29:16 +0800

    Enter

---
+++ hello.py
@@ -0,0 +1,13 @@
+# # # numbers=list(range(1,6))
+# # # print(numbers)
+# # numbers= range(1,11)
+# # for number in numbers:
+# #     print(number**3)
+# number = [value**3 for value in range(1,11)]
+# print(number)
+# print(number[1:])
+# print(number[:4])
+# print(number[1:3])
+
+car="audi"
+print(car=="bmw")
