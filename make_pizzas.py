2b899a5737810c8c779729fa12dc56d6fce1d9f1
haha110110 <boyang.afma@gmail.com>
2021-08-18 21:46:30 +0800

    master

--- make_pizzas.py
+++ make_pizzas.py
@@ -1,4 +1,4 @@
-import pizza
+from pizza import *
 
-pizza.make_pizza(16, 'pepperoni')
-pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
